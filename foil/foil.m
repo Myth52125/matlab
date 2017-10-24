517df2a6072db1f46aaff1d5e3bf06e475c2104a
Myth52125 <myth52125@hotmail.com>
2017-10-25 06:56:23 +0800

    add commet

--- foil/foil.m
+++ foil/foil.m
@@ -1,8 +1,12 @@
-%q1ÎªÄÚ²àÅòÕÍÁ¦
-%RÎªÍâÔ²°ë¾¶
-%³¤¶Èµ¥Î»ºÁÃ×
+%q1ï¼šå†…ä¾§è†¨èƒ€åŠ›
+%R:å¤–åœ†åŠå¾„
+%r:å†…åœ†åŠå¾„
+%d:åœ†å¿ƒåˆ°åº”å˜ç‰‡çš„å‚ç›´è·ç¦»
+%l:åº”å˜ç‰‡é•¿åº¦
+%step:è®¡ç®—çš„æ­¥é•¿ï¼Œl/step = æ¯ä»½çš„é•¿åº¦
+
 function totalr = foil( q1,R)
-%E£º¡Á10^4N/mm^2
+%Eå¼¹æ€§æ¨¡é‡å•ä½10^4N/mm^2
 E=3*10^4; u=0.16; d=20; l=10;q2=0;
 result =[];
 rresult=[];
@@ -11,24 +15,24 @@ step = 0.1;
 n=l/step;
 for dx=0:step:l
     
-    %¼ÆËã¸÷¸ö²ÎÊı
+    %è®¡ç®—å„ä¸ªé•¿åº¦
     r=sqrt(d^2+dx^2); 
     A=(r^2 * R^2*(q2-q1))/(R^2-r^2);
     C=(q1*r^2-q2*R^2)/(R^2-r^2);
     angle = atand(dx/d);
 
-    %¼ÆËãÎ»ÒÆ·ÖÁ¿
+    %ï¿½ï¿½ï¿½ï¿½Î»ï¿½Æ·ï¿½ï¿½ï¿½
     tmpu=getup(E,A,C,u,r)*sind(angle);  
     result=[result;tmpu];    
     
-    %¼ÆËãµç×è
+    %å åŠ å„ä¸ªå°æ®µçš„ç”µé˜»
     totalr =totalr + ((step+tmpu)^2)/(step*l);    
 end
 plot(result);
 totalr = totalr-1 ;
 end
 
-%¼ÆËãÒ»¸öµãµÄup
+%è®¡ç®—åº”å˜çš„
 function up=getup(E,A,C,u,r)
 up=(1/E)*(-(1+u)*A/r+2*(1-u)*C*r);
 end
